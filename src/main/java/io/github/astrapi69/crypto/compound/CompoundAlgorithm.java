98950bc1ea47f33ae9a5b02f0e0036a64692d85f
astrapi69 <asterios.raptis@gmx.net>
2021-06-17 12:22:26 +0200

    replaced name of enum value CompoundAlgorithm#PBE_WITH_MD5_AND_DES with SunJCEAlgorithm.PBEWithMD5AndDES.name()

--- src/main/java/io/github/astrapi69/crypto/compound/CompoundAlgorithm.java
+++ src/main/java/io/github/astrapi69/crypto/compound/CompoundAlgorithm.java
@@ -1,8 +1,8 @@
 /**
  * The MIT License
- *
+ * <p>
  * Copyright (C) 2015 Asterios Raptis
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -10,10 +10,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -45,58 +45,59 @@ public enum CompoundAlgorithm implements Algorithm
 	/**
 	 * The enum constant for the 'PBEWithMD5AndAES' algorithm
 	 */
-	PBE_WITH_MD5_AND_AES(PBEMechanism.PBE.name() + UnionWord.With.name() + MdAlgorithm.MD5.name()
-		+ UnionWord.And.name() + AesAlgorithm.AES.name()),
+	PBE_WITH_MD5_AND_AES(
+		PBEMechanism.PBE.name() + UnionWord.With.name() + MdAlgorithm.MD5.name() + UnionWord.And
+			.name() + AesAlgorithm.AES.name()),
 
 	/**
 	 * The enum constant for the 'PBEWithMD5AndDES' algorithm
 	 */
-	PBE_WITH_MD5_AND_DES(PBEMechanism.PBE.name() + UnionWord.With.name() + MdAlgorithm.MD5.name()
-		+ UnionWord.And.name() + SunJCEAlgorithm.DES.name()),
+	PBE_WITH_MD5_AND_DES(SunJCEAlgorithm.PBEWithMD5AndDES.name()),
 
 	/**
 	 * The enum constant for the 'PBEWITHSHA1AND128BITAES-CBC-BC' algorithm
 	 */
 	PBE_WITH_SHA1_AND_128BIT_AES_CBC_BC(
-		PBEMechanism.PBE.name() + UnionWord.With.name().toUpperCase() + HashAlgorithm.SHA1.name()
-			+ UnionWord.And.name().toUpperCase() + "128BIT" + AesAlgorithm.AES.name() + "-"
-			+ Mode.CBC.name() + "-" + Mode.BC.name()),
+		PBEMechanism.PBE.name() + UnionWord.With.name().toUpperCase() + HashAlgorithm.SHA1
+			.name() + UnionWord.And.name().toUpperCase() + "128BIT" + AesAlgorithm.AES
+			.name() + "-" + Mode.CBC.name() + "-" + Mode.BC.name()),
 
 	/**
 	 * The enum constant for the 'PBEWithSHA1AndDESede' algorithm
 	 */
-	PBE_WITH_SHA1_AND_DES_EDE(PBEMechanism.PBE.name() + UnionWord.With.name()
-		+ HashAlgorithm.SHA1.name() + UnionWord.And.name() + SunJCEAlgorithm.DESede.name()),
+	PBE_WITH_SHA1_AND_DES_EDE(
+		PBEMechanism.PBE.name() + UnionWord.With.name() + HashAlgorithm.SHA1.name() + UnionWord.And
+			.name() + SunJCEAlgorithm.DESede.name()),
 
 	/**
 	 * The enum constant for the 'PBKDF2WithHmacSHA1' algorithm
 	 */
-	PBKDF2_WITH_HMAC_SHA1(PasswordHashType.PBKDF2.name() + UnionWord.With.name() + MacAlgorithm.HMAC
-		+ HashAlgorithm.SHA1.name()),
+	PBKDF2_WITH_HMAC_SHA1(PasswordHashType.PBKDF2.name() + UnionWord.With
+		.name() + MacAlgorithm.HMAC + HashAlgorithm.SHA1.name()),
 
 	/**
 	 * The enum constant for the 'SHA1withRSA' algorithm
 	 */
-	SHA1_WITH_RSA(HashAlgorithm.SHA1.getAlgorithm() + UnionWord.With.name().toLowerCase()
-			+ KeyPairGeneratorAlgorithm.RSA.getAlgorithm()),
+	SHA1_WITH_RSA(HashAlgorithm.SHA1.getAlgorithm() + UnionWord.With.name()
+		.toLowerCase() + KeyPairGeneratorAlgorithm.RSA.getAlgorithm()),
 
 	/**
 	 * The enum constant for the 'SHA256withRSA' algorithm
 	 */
-	SHA256_WITH_RSA(HashAlgorithm.SHA256.getAlgorithm() + UnionWord.With.name().toLowerCase()
-			+ KeyPairGeneratorAlgorithm.RSA.getAlgorithm()),
+	SHA256_WITH_RSA(HashAlgorithm.SHA256.getAlgorithm() + UnionWord.With.name()
+		.toLowerCase() + KeyPairGeneratorAlgorithm.RSA.getAlgorithm()),
 
 	/**
 	 * The enum constant for the 'SHA384withRSA' algorithm
 	 */
-	SHA384_WITH_RSA(HashAlgorithm.SHA384.getAlgorithm() + UnionWord.With.name().toLowerCase()
-			+ KeyPairGeneratorAlgorithm.RSA.getAlgorithm()),
+	SHA384_WITH_RSA(HashAlgorithm.SHA384.getAlgorithm() + UnionWord.With.name()
+		.toLowerCase() + KeyPairGeneratorAlgorithm.RSA.getAlgorithm()),
 
 	/**
 	 * The enum constant for the 'SHA512withRSA' algorithm
 	 */
-	SHA512_WITH_RSA(HashAlgorithm.SHA512.getAlgorithm() + UnionWord.With.name().toLowerCase()
-			+ KeyPairGeneratorAlgorithm.RSA.getAlgorithm());
+	SHA512_WITH_RSA(HashAlgorithm.SHA512.getAlgorithm() + UnionWord.With.name()
+		.toLowerCase() + KeyPairGeneratorAlgorithm.RSA.getAlgorithm());
 
 
 	/**
@@ -113,7 +114,7 @@ public enum CompoundAlgorithm implements Algorithm
 	 * Constant array for the default contents of salt
 	 */
 	public static final byte[] SALT = { (byte)0xA9, (byte)0x9B, (byte)0xC8, (byte)0x32, (byte)0x56,
-			(byte)0x35, (byte)0xE3, (byte)0x03 };
+		(byte)0x35, (byte)0xE3, (byte)0x03 };
 
 	/** The algorithm. */
 	private final String algorithm;
@@ -129,9 +130,19 @@ public enum CompoundAlgorithm implements Algorithm
 		this.algorithm = algorithm;
 	}
 
-	@Override
-	public String getAlgorithm()
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override public String getAlgorithm()
 	{
 		return this.algorithm;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override public String toString()
+	{
+		return getAlgorithm();
 	}
 }
